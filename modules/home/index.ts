2c0b4a5b54ab34ca9255b287f4999260b876de2c
vocoWone <605131105@qq.com>
2019-11-23 18:22:32 +0800

    Update demo

--- modules/home/index.ts
+++ modules/home/index.ts
@@ -26,7 +26,11 @@ class ActionHandler extends Model<State> {
   }
 
   async text() {
-    this.setState({ name: "click button name" });
+    console.info(`next context: ${this.context}`); // 获取next context
+    console.info(`model state: ${this.state}`); // 当前模块的state
+    console.info(`root state: ${this.rootState}`); // 整个项目的state
+    this.setState({ name: "new name" }); // 更新 model state
+    this.resetState(); // 重置当前model的state
   }
 }
 
